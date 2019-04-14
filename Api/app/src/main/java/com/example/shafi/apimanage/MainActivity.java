ff61c31c50739f1dc9f7e050b10ebdab4151de12
Shafi-Raive <shafiraive13@gmail.com>
2019-04-14 19:28:54 +0600

    first commit

---
+++ Api/app/src/main/java/com/example/shafi/apimanage/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.shafi.apimanage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btBtudent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        btBtudent = findViewById(R.id.btStudent);
+
+        btBtudent.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        
+    }
+}
